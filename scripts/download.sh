2d978dac645b6f8aaadf2e2b02fee0f3f2d24507
Anthony Corletti <anthcor@gmail.com>
2023-01-07 16:57:29 -0500

    scripting

--- scripts/download.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh -ex
-
-pip install .
-
-scdl -l https://soundcloud.com/corletti/sets/barely0 --path /Users/anthony/Music --strict-playlist
-scdl -l https://soundcloud.com/corletti/sets/barely1 --path /Users/anthony/Music --strict-playlist
-scdl -l https://soundcloud.com/corletti/sets/barely2 --path /Users/anthony/Music --strict-playlist
-
